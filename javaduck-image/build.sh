128a14c8cbb298084d955f94ce030d90578b8548
Craig Jones <Craig.Jones@utas.edu.au>
2017-08-25 12:50:52 +1000

    Use xenial/packeged netcdf for base aggregator image
    Remove old version of aws sdk included with netcdfAll so ECS task roles work consistently.

--- javaduck-image/build.sh
+++ javaduck-image/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 eval "$(aws ecr get-login --no-include-email --region us-east-1)"
 docker build -t javaduck .
 docker tag javaduck:latest 104044260116.dkr.ecr.us-east-1.amazonaws.com/javaduck:latest
