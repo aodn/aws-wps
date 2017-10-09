d85eadebde96544388dfeef7b1d482b8e959c687
ccmoloney <cameron.moloney@utas.edu.au>
2017-10-09 12:22:40 +1100

    Generate provenance record (#55)
    * #697:  Add mechanism to generate the provenance record for aggregations.
    
    * #697: Updated SubsetParameters implementation to match current WPS implementation - to match provenance templates.
    
    * #697: Fix for GHRSST layer (and possibly others) not working.

--- aggregation-worker/src/main/java/au/org/emii/geoserver/client/SubsetParameters.java
+++ aggregation-worker/src/main/java/au/org/emii/geoserver/client/SubsetParameters.java
@@ -1,33 +1,108 @@
 package au.org.emii.geoserver.client;
 
-import java.util.ArrayList;
+import ucar.ma2.Range;
+import ucar.nc2.time.CalendarDate;
+import ucar.nc2.time.CalendarDateRange;
+import ucar.unidata.geoloc.LatLonPointImpl;
+import ucar.unidata.geoloc.LatLonRect;
+
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
-import au.org.emii.geoserver.client.SubsetParameters.SubsetParameter;
+public class SubsetParameters {
+    public static final String LATITUDE = "LATITUDE";
+    public static final String LONGITUDE = "LONGITUDE";
+    public static final String TIME = "TIME";
+    private final LatLonRect bbox;
+    private final CalendarDateRange timeRange;
 
-public class SubsetParameters extends HashMap<String, SubsetParameter> {
-    public class SubsetParameter {
-        public String start;
-        public String end;
+    public SubsetParameters(LatLonRect bbox, CalendarDateRange timeRange) {
+        this.bbox = bbox;
+        this.timeRange = timeRange;
+    }
 
-        public SubsetParameter(String start, String end) {
+    public LatLonRect getBbox() {
+        return bbox;
+    }
+
+    public boolean isPointSubset() {
+        return bbox.getLowerLeftPoint().equals(bbox.getUpperRightPoint());
+    }
+
+    public CalendarDateRange getTimeRange() {
+        return timeRange;
+    }
+
+    public Range getVerticalRange() {
+        return null;
+    }
+
+    public static SubsetParameters parse(String subset) {
+
+        Double latMin, latMax, lonMin, lonMax;
+        Map<String, ParameterRange> subsets = new HashMap<>();
+        String latLonErrorMsg = String.format("Invalid latitude/longitude format for subset: %s Valid latitude/longitude format example: LATITUDE,-33.433849,-32.150743;LONGITUDE,114.15197,115.741219", subset);
+        String timeErrorMsg = String.format("Invalid time format for subset: %s Valid time format example: TIME,2009-01-01T00:00:00.000Z,2009-12-25T23:04:00.000Z", subset);
+        String subsetErrorMsg = String.format("Invalid format for subset: %s Valid format example: TIME,2009-01-01T00:00:00.000Z,2009-12-25T23:04:00.000Z;LATITUDE,-33.433849,-32.150743;LONGITUDE,114.15197,115.741219", subset);
+
+        // Parse
+        for (String part : subset.split(";")) {
+            String[] subsetParts = part.split(",");
+            try {
+                subsets.put(subsetParts[0], new ParameterRange(subsetParts[1], subsetParts[2]));
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new RuntimeException(String.format("%s error: Invalid subset parameter '%s'", subsetErrorMsg, part));
+            }
+        }
+
+        for (String key: subsets.keySet()) {
+            if (!key.equals(LATITUDE) && !key.equals(LONGITUDE) && !key.equals(TIME)) {
+                throw new RuntimeException(String.format("%s error: Invalid subset parameter '%s'", subsetErrorMsg, key));
+            }
+        }
+
+        ParameterRange latitudeRange = subsets.get(LATITUDE);
+        ParameterRange longitudeRange = subsets.get(LONGITUDE);
+
+        if (latitudeRange == null || longitudeRange == null) {
+            throw new RuntimeException(latLonErrorMsg);
+        }
+
+        try {
+            latMin = Double.parseDouble(latitudeRange.start);
+            latMax = Double.parseDouble(latitudeRange.end);
+            lonMin = Double.parseDouble(longitudeRange.start);
+            lonMax = Double.parseDouble(longitudeRange.end);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException(String.format("%s error: '%s'", latLonErrorMsg, e.getMessage()));
+        }
+
+        LatLonRect bbox = new LatLonRect(new LatLonPointImpl(latMin, lonMin), new LatLonPointImpl(latMax, lonMax));
+
+        ParameterRange timeRange = subsets.get(TIME);
+        CalendarDateRange calendarDateRange = null;
+
+        // Time Validation
+        if (timeRange != null) {
+            try {
+                CalendarDate startTime = CalendarDate.parseISOformat("gregorian", timeRange.start);
+                CalendarDate endTime = CalendarDate.parseISOformat("gregorian", timeRange.end);
+                calendarDateRange = CalendarDateRange.of(startTime, endTime);
+            } catch (IllegalArgumentException e) {
+                throw new RuntimeException(String.format("%s error: '%s'", timeErrorMsg, e.getMessage()));
+            }
+        }
+
+        return new SubsetParameters(bbox, calendarDateRange);
+    }
+
+    private static class ParameterRange {
+        String start;
+        String end;
+
+        ParameterRange(String start, String end) {
             this.start = start;
             this.end = end;
         }
     }
-
-    public SubsetParameters(String subset) {
-        super();
-        for (String part : subset.split(";")) {
-            String[] subsetParts = part.split(",");
-            put(subsetParts[0], new SubsetParameter(subsetParts[1], subsetParts[2]));
-        }
-    }
-
-    // Simple copy ctor
-    public SubsetParameters(SubsetParameters sp) {
-        super(sp);
-    }
-
 }
--- wps-common/src/main/java/au/org/aodn/aws/util/S3Utils.java
+++ wps-common/src/main/java/au/org/aodn/aws/util/S3Utils.java
@@ -1,30 +1,35 @@
 package au.org.aodn.aws.util;
 
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+import com.amazonaws.util.StringInputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import thredds.crawlabledataset.s3.S3URI;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 public class S3Utils {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(S3Utils.class);
 
-    public static String readS3ObjectAsString(String s3Bucket, String s3Key, String s3Region) throws IOException
-    {
+    public static String readS3ObjectAsString(String s3Bucket, String s3Key, String s3Region) throws IOException {
         String objectString = null;
         S3ObjectInputStream contentStream = getS3ObjectStream(s3Bucket, s3Key, s3Region);
 
         //  read file to String
         try {
             objectString = Utils.inputStreamToString(contentStream);
-        }
-        catch(IOException ioex)
-        {
+        } catch (IOException ioex) {
             //  Bad stuff - blow up!
             LOGGER.error("Problem loading S3 object: ", ioex);
             throw ioex;
@@ -33,11 +38,10 @@ public class S3Utils {
     }
 
 
-    public static S3ObjectInputStream getS3ObjectStream(String s3Bucket, String s3Key, String s3Region) throws IOException
-    {
+    public static S3ObjectInputStream getS3ObjectStream(String s3Bucket, String s3Key, String s3Region) throws IOException {
         //  Get from S3 bucket location
         AmazonS3Client s3Client = new AmazonS3Client();
-        if(s3Region != null) {
+        if (s3Region != null) {
             Region region = Region.getRegion(Regions.fromName(s3Region));
             s3Client.setRegion(region);
         }
@@ -45,5 +49,31 @@ public class S3Utils {
         S3Object templateObject = s3Client.getObject(s3Bucket, s3Key);
         return templateObject.getObjectContent();
 
+    }
+
+
+    public static void uploadToS3(S3URI s3URI, File file)
+            throws InterruptedException {
+        DefaultAWSCredentialsProviderChain credentialProviderChain = new DefaultAWSCredentialsProviderChain();
+        TransferManager tx = new TransferManager(credentialProviderChain.getCredentials());
+        Upload myUpload = tx.upload(s3URI.getBucket(), s3URI.getKey(), file);
+        myUpload.waitForCompletion();
+        tx.shutdownNow();
+    }
+
+
+    public static void uploadToS3(S3URI s3URI, String content)
+            throws InterruptedException, IOException {
+        DefaultAWSCredentialsProviderChain credentialProviderChain = new DefaultAWSCredentialsProviderChain();
+        TransferManager tx = new TransferManager(credentialProviderChain.getCredentials());
+        try {
+            StringInputStream stringStream = new StringInputStream(content);
+            Upload myUpload = tx.upload(s3URI.getBucket(), s3URI.getKey(), stringStream, null);
+            myUpload.waitForCompletion();
+            tx.shutdownNow();
+        } catch (UnsupportedEncodingException ex) {
+            LOGGER.error("Unable to upload content to S3 : " + ex.getMessage(), ex);
+            throw new IOException("Unable to upload content to S3 : " + ex.getMessage(), ex);
+        }
     }
 }
