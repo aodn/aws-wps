fdc7c9ab9114a4d495cb0ea617e0fdbcc456fbb9
Craig Jones <Craig.Jones@utas.edu.au>
2017-10-17 09:18:57 +1100

    Set content-type appropriately on response Use standard mimetype strings for requested format Fix compile error

--- job-status-service/src/main/java/au/org/aodn/aws/wps/JobStatusFormatEnum.java
+++ job-status-service/src/main/java/au/org/aodn/aws/wps/JobStatusFormatEnum.java
@@ -1,6 +1,17 @@
 package au.org.aodn.aws.wps;
 
 public enum JobStatusFormatEnum {
-    XML,
-    HTML;
+    XML("application/xml"),
+    HTML("text/html");
+
+    private final String mimeType;
+
+    JobStatusFormatEnum(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String mimeType() {
+        return mimeType;
+    }
+
 }
