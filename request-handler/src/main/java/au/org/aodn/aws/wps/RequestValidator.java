67ddbcdf8352339e21ba82b768e1774b0666be72
Craig Jones <Craig.Jones@utas.edu.au>
2017-10-19 13:59:55 +1100

    Fixes #74 - Set Content-Type for Execute, DescribeProcess and GetCapabilities responses (#79)

--- request-handler/src/main/java/au/org/aodn/aws/wps/RequestValidator.java
+++
@@ -1,7 +0,0 @@
-package au.org.aodn.aws.wps;
-
-import au.org.aodn.aws.wps.exception.ValidationException;
-
-public interface RequestValidator {
-    void validate(AwsApiRequest request) throws ValidationException;
-}
