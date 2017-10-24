c7aea65a1a825b8d87c67fc07720e67b22aef1d6
Craig Jones <Craig.Jones@utas.edu.au>
2017-10-24 16:35:52 +1100

    Add some integration tests for AWS WPS (#90)
    * Add some integration tests for AWS WPS
    
    Need to specify WPS_ENDPOINT as an environment variable.
    
    * Turn of execute response validation until its made to validate

---
+++ integration-testing/src/test/java/au/org/emii/wps/it/DescribeProcessIT.java
@@ -0,0 +1,63 @@
+package au.org.emii.wps.it;
+
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.filter.log.RequestLoggingFilter;
+import com.jayway.restassured.filter.log.ResponseLoggingFilter;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.specification.RequestSpecification;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static au.org.emii.wps.util.Matchers.validateWith;
+
+public class DescribeProcessIT {
+    private static final String SERVICE_ENDPOINT = System.getenv("WPS_ENDPOINT");
+
+    private static RequestSpecification spec;
+
+    @BeforeClass
+    public static void initSpec() {
+        spec = new RequestSpecBuilder()
+            .setBaseUri(SERVICE_ENDPOINT)
+            .addParameter("service", "WPS")
+            .addParameter("request", "DescribeProcess")
+            .addFilter(new ResponseLoggingFilter())
+            .addFilter(new RequestLoggingFilter())
+            .build();
+    }
+
+    @Test
+    public void testDescribeUnknownProcess() throws IOException {
+        given()
+            .spec(spec)
+            .param("identifier", "unknown")
+        .when()
+            .get()
+        .then()
+            .statusCode(500)
+            .contentType(ContentType.XML)
+            .body(validateWith("/ows/1.1.0/owsAll.xsd"))
+            .root("ExceptionReport.Exception")
+            .body("@exceptionCode", equalTo("InvalidParameterValue"))
+            .body("@locator", equalTo("identifier"))
+            .body("ExceptionText", equalTo("No such process 'unknown'"));
+    }
+
+    @Test
+    public void testDescribeKnownProcess() {
+        given()
+            .spec(spec)
+            .param("identifier", "gs:GoGoDuck")
+        .when()
+            .get()
+        .then()
+            .statusCode(200)
+            .contentType(ContentType.XML)
+            .body(validateWith("/wps/1.0.0/wpsAll.xsd"));
+    }
+
+}
