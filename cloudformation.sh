1223e1803e2f112c3569cf9050f2d63b92db1883
Craig Jones <Craig.Jones@utas.edu.au>
2018-01-05 11:26:09 +1100

    Allow aws profile to use to be specified in cloudformation script

--- cloudformation.sh
+++ cloudformation.sh
@@ -3,7 +3,8 @@
 set -eu
 
 function get_account_id() {
-    local account_id=$(aws sts get-caller-identity --output text --query Account)
+    local aws_profile=$1
+    local account_id=$(aws --profile $aws_profile sts get-caller-identity --output text --query Account)
     echo -n ${account_id}
 }
 
@@ -24,19 +25,25 @@ function get_cache_bucket_for_account() {
 }
 
 function usage() {
-    echo "Usage: $0 STACK_NAME PROPERTIES_FILE"
+    echo "Usage: $0 [AWS_PROFILE] STACK_NAME PROPERTIES_FILE"
     exit 1
 }
 
 function main() {
-    # parameter validation
-    [ $# -ne 2 ] && usage
+    # parameter parsing
+    [[ $# -lt 2 || $# -gt 3 ]] && usage
+    local aws_profile
+    if [[ $# -eq 3 ]] ; then
+      aws_profile=$1; shift
+    else
+      aws_profile="default"
+    fi
     local stack_name=$1; shift
     local properties_file=$1; shift
     [ ! -e ${properties_file} ] && usage
 
     # resolve the appropriate cache bucket based on the account ID of the current credentials
-    account_id=$(get_account_id)
+    account_id=$(get_account_id $aws_profile)
     cache_bucket=$(get_cache_bucket_for_account ${account_id})
 
     if [ "z${cache_bucket}" == "z" ]; then
@@ -48,8 +55,8 @@ function main() {
     temporary_template=$(mktemp --tmpdir=. --suffix=.yaml)
     trap "rm -f ${temporary_template}" EXIT
 
-    aws cloudformation package --template-file ./wps-cloudformation-template.yaml --s3-bucket ${cache_bucket} --s3-prefix lambda --output-template-file ${temporary_template}
-    aws cloudformation deploy --template-file ${temporary_template} --stack-name ${stack_name} --parameter-overrides $(cat ${properties_file}) --capabilities CAPABILITY_IAM
+    aws --profile ${aws_profile} cloudformation package --template-file ./wps-cloudformation-template.yaml --s3-bucket ${cache_bucket} --s3-prefix lambda --output-template-file ${temporary_template}
+    aws --profile ${aws_profile} cloudformation deploy --template-file ${temporary_template} --stack-name ${stack_name} --parameter-overrides $(cat ${properties_file}) --capabilities CAPABILITY_IAM
 }
 
 main $@
