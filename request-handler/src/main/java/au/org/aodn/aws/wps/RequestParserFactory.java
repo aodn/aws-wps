18db7e0f08281d3d7bea04d4ddf71e28a8dee2f5
pmbohm <pmbohm@utas.edu.au>
2019-08-26 15:15:46 +1000

    Logging null parameters/headers

--- request-handler/src/main/java/au/org/aodn/aws/wps/RequestParserFactory.java
+++ request-handler/src/main/java/au/org/aodn/aws/wps/RequestParserFactory.java
@@ -20,26 +20,35 @@ public class RequestParserFactory  {
             return new QueryStringParameterParser(request);
         }
         else {
-            Map<String, String> headers = request.getHeaders();
-
             try  {
+
                 // TODO: handle as required by spec
                 Map<String, String> parameters = request.getQueryStringParameters();
-                for (String key : parameters.keySet()) {
-                    logger.error("  - Query parameter: Key [" + key + "], Value [" + parameters.get(key) + "]");
+                if (parameters != null) {
+                    for (String key : parameters.keySet()) {
+                        logger.error("  - Query parameter: Key [" + key + "], Value [" + parameters.get(key) + "]");
+                    }
+                }
+                else {
+                    logger.error("  - Query parameters are null")
                 }
             }
             catch (Exception e){
                 throw new InvalidRequestException(e);
             }
 
-            //  Log some debugging information
-            logger.error("Unsupported HTTP method invoked: " + request.getHttpMethod());
-
-            for (String key : headers.keySet()) {
-                logger.error("  - Query header: Key [" + key + "], Value [" + headers.get(key) + "]");
+            Map<String, String> headers = request.getHeaders();
+            if (headers != null) {
+                for (String key : headers.keySet()) {
+                    logger.error("  - Query header: Key [" + key + "], Value [" + headers.get(key) + "]");
+                }
+            }
+            else {
+                logger.error("  - Query headers are null")
             }
 
+            //  Log some debugging information
+            logger.error("Unsupported HTTP method invoked: " + request.getHttpMethod());
             logger.error("BODY: [" + request.getBody() + "]");
 
             throw new UnsupportedOperationException(request.getHttpMethod() + " not supported");
